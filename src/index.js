86b839627c1df1d7d50ad986c5c9739c35d836b2
Velimir Krstic <velimir.k@hotmail.com>
2019-10-22 12:48:35 +0200

    Dodat kostur projekta

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//Nedelja1, dan 1
+/* Danas smo presli osnove HTML-a, CSS-a, JS-a;
+Sledeci kod nam ispisuje poruku kao i u web stranicu ubacuje neki element
+*/
+console.log('Hello by js');
+console.log("Hello World!")
+
+document.getElementById("app").innerHTML="<p>Hello to BootcampPizza from Javascript!</p>";
+
+
